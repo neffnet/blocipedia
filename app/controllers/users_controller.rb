36d4dc4252851e79440d5f1a2cb2c26858426094
neffnet <neff@neffnet.net>
2015-05-10 23:51:33 -0500

    fix to let devise accept user name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
-  end
 end
