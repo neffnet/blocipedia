7fa911fe93ba18c373a96b1d172a00015d0039cc
neffnet <neff@neffnet.net>
2015-05-10 21:54:35 -0500

    hopefully devise now authenticates with username rather than email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  protected
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value", {:value => login.downcase}]).first
+  end
 end
