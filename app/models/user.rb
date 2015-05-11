6f70e7bb6a48e7c2421520acbf0e5a19fd0214d4
neffnet <neff@neffnet.net>
2015-05-10 22:53:27 -0500

    add devise views, login form in header

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  protected
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value", {:value => login.downcase}]).first
-  end
 end
