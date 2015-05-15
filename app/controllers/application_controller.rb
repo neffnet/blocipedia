cb2c8a372eb0ad9c99ae1d049d34191844971a6c
neffnet <neff@neffnet.net>
2015-05-15 15:33:21 -0500

    do not redirect after bad login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    request.original_url
+    # user_path(current_user)
   end
 
   protected
