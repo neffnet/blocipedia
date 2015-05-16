c2a7886d915010389cb50267ca220344586356b6
neffnet <neff@neffnet.net>
2015-05-15 21:37:11 -0500

    users can upgrade from user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @wikis = @user.wikis
+
+    @stripe_btn_data = {
+      key: "#{ENV['STRIPE_PUBLISHABLE_KEY']}",
+      description: "Upgrade to Premium",
+      amount: Amount.default
+    }
   end
 end
